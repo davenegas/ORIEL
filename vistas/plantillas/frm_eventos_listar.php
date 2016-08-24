a872c3e6faab6cf060e07cffaa4d44d5c6660f62
davenegas <diego20venegas@gmail.com>
2016-08-24 09:09:35 -0600

    revisión eventos

--- vistas/plantillas/frm_eventos_listar.php
+++ vistas/plantillas/frm_eventos_listar.php
@@ -9,28 +9,28 @@
         <?php require_once 'encabezado.php';?>
         <div class="container animated fadeIn">
         <h2>Listado de Eventos</h2>
-        <!--<p>A continuación se detallan los diferentes roles que están registrados en el sistema:</p>-->            
+        <!--<p>A continuación se detallan los diferentes roles que están registrados en el sistema:</p>-->   
+        <a href="index.php?ctl=frm_eventos_agregar&id=0" class="btn btn-default espacio-abajo" role="button">Agregar Nuevo Evento de Bitácora</a>
+        <a href="index.php?ctl=frm_eventos_lista_cerrados" class="btn btn-default espacio-abajo" role="button">Eventos Cerrados</a> 
         <table id="tabla" class="display">
           <thead>
             <tr>
-              <th>Fecha</th>
-              <th>Hora</th>
-              <th>Lapso</th>
-              <th>Provincia</th>
-              <th>Tipo Punto</th>
-              <th>Punto BCR</th>
-              <th>Tipo de Evento</th>
-              <th>Estado del Evento</th>
-              <th>Ingresado Por</th>
-              <th>Editar Evento</th>
+                <th>ID Evento</th>
+                <th>Fecha</th>
+                <th>Hora</th>
+                <th>Lapso</th>
+                <th>Provincia</th>
+                <th>Tipo Punto</th>
+                <th>Punto BCR</th>
+                <th>Tipo de Evento</th>
+                <th>Estado del Evento</th>
+                <th>Ingresado Por</th>
+                <th>Editar Evento</th>
             </tr>
           </thead>
           <tbody>
-        
             <?php 
-
             $tam=count($params);
-
             for ($i = 0; $i <$tam; $i++) {
             ?>
             <tr>
@@ -39,25 +39,24 @@
             $fecha_actual = date_create(date("d-m-Y"));
             $dias_abierto= date_diff($fecha_evento, $fecha_actual);
             ?>
-            <td><?php echo date_format($fecha_evento, 'd/m/Y');?></td>
-            <td><?php echo $params[$i]['Hora'];?></td>
-            <td align="center"><?php echo $dias_abierto->format('%a');?></td>
-            <td><?php echo $params[$i]['Nombre_Provincia'];?></td>
-            <td><?php echo $params[$i]['Tipo_Punto'];?></td>
-            <td><?php echo $params[$i]['Nombre'];?></td>
-            <td><?php echo $params[$i]['Evento'];?></td>
-            <td><?php echo $params[$i]['Estado_Evento'];?></td>
-            <td><?php echo $params[$i]['Nombre_Usuario']." ".$params[$i]['Apellido'] ?></td>
-            <td align="center"><a href="index.php?ctl=frm_eventos_editar&accion=editar_abiertos&id=
-               <?php echo $params[$i]['ID_Evento']?>">Gestionar Seguimiento</a></td>
+                <td><?php echo $params[$i]['ID_Evento'];?></td>
+                <td><?php echo date_format($fecha_evento, 'd/m/Y');?></td>
+                <td><?php echo $params[$i]['Hora'];?></td>
+                <td align="center"><?php echo $dias_abierto->format('%a');?></td>
+                <td><?php echo $params[$i]['Nombre_Provincia'];?></td>
+                <td><?php echo $params[$i]['Tipo_Punto'];?></td>
+                <td><?php echo $params[$i]['Nombre'];?></td>
+                <td><?php echo $params[$i]['Evento'];?></td>
+                <td><?php echo $params[$i]['Estado_Evento'];?></td>
+                <td><?php echo $params[$i]['Nombre_Usuario']." ".$params[$i]['Apellido'] ?></td>
+                <td align="center"><a href="index.php?ctl=frm_eventos_editar&accion=editar_abiertos&id=
+                   <?php echo $params[$i]['ID_Evento']?>">Gestionar Seguimiento</a></td>
             </tr>
             <?php }
             ?>
             </tbody>
         </table>
-        <a href="index.php?ctl=frm_eventos_agregar&id=0" class="btn btn-default" role="button">Agregar Nuevo Evento de Bitácora</a>
-        <a href="index.php?ctl=frm_eventos_lista_cerrados" class="btn btn-default" role="button">Eventos Cerrados</a> 
         </div>
-            <?php require 'vistas/plantillas/pie_de_pagina.php' ?>
+        <?php require 'vistas/plantillas/pie_de_pagina.php' ?>
     </body>
 </html>
