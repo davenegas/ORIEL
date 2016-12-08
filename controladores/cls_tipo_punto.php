453a9ecf1059c8241a5d654bbf89830b9dba23ec
AllanQuesada <allanq35@gmail.com>
2016-12-08 15:55:12 -0600

    Prueba

---
+++ controladores/cls_tipo_punto.php
@@ -0,0 +1,284 @@
+<?php
+ class cls_tipo_punto{
+    public $id;
+    public $id2;
+    public $obj_data_provider;
+    public $arreglo;
+    public $arreglo2;
+    private$condicion;
+    public $estado;
+    public $observaciones;
+    public $tipo;
+    public $numero_tp; 
+    public $id_ultima_tp_ingresada;
+    
+    
+    function getArreglo2() {
+        return $this->arreglo2;
+    }
+
+    function setArreglo2($arreglo2) {
+        $this->arreglo2 = $arreglo2;
+    }
+
+        function getId_ultima_tp_ingresada() {
+        return $this->id_ultima_tp_ingresada;
+    }
+
+    function setId_ultima_tp_ingresada($id_ultima_tp_ingresada) {
+        $this->id_ultima_tp_ingresada = $id_ultima_tp_ingresada;
+    }
+    
+    function getId() {
+        return $this->id;
+    }
+
+    function getId2() {
+        return $this->id2;
+    }
+
+    function getObj_data_provider() {
+        return $this->obj_data_provider;
+    }
+
+    function getArreglo() {
+        return $this->arreglo;
+    }
+
+    function getCondicion() {
+        return $this->condicion;
+    }
+
+    function getEstado() {
+        return $this->estado;
+    }
+
+    function getObservaciones() {
+        return $this->observaciones;
+    }
+
+    function getTipo_Punto() {
+        return $this->tipo;
+    }
+
+    function getID_Tipo_Punto() {
+        return $this->numero_tp;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setId2($id2) {
+        $this->id2 = $id2;
+    }
+
+    function setObj_data_provider($obj_data_provider) {
+        $this->obj_data_provider = $obj_data_provider;
+    }
+
+    function setArreglo($arreglo) {
+        $this->arreglo = $arreglo;
+    }
+
+    function setCondicion($condicion) {
+        $this->condicion = $condicion;
+    }
+
+    function setEstado($estado) {
+        $this->estado = $estado;
+    }
+
+    function setObservaciones($observaciones) {
+        $this->observaciones = $observaciones;
+    }
+
+    function setTipo_Punto($tipo) {
+        $this->tipo = $tipo;
+    }
+
+    function setID_Tipo_Punto($numero_tp) {
+        $this->numero_tp = $numero_tp;
+    }
+
+    public function __construct() {
+        
+        $this->id="";
+        $this->id2="";
+        $this->condicion="";
+        $this->arreglo;
+        $this->obj_data_provider=new Data_Provider();
+        $this->observaciones="";
+        $this->estado="";
+        $this->tipo="";
+        $this->numero_tp=""; 
+    }
+    
+    //Obtener el último id de evento para saber que se debe ingresar
+    function obtiene_id_tp_por_nombre(){
+    //Establece la conexión con la bd
+      $this->obj_data_provider->conectar();
+      
+      $this->obj_data_provider->trae_datos("t_tipopuntobcr","ID_Tipo_Punto","Tipo_Punto='".$this->tipo."'");
+      
+      $this->arreglo=$this->obj_data_provider->getArreglo();
+     
+      $this->obj_data_provider->desconectar();
+      
+      if (count($this->arreglo)>0){
+          $this->setId($this->arreglo[0]['ID_Tipo_Punto']);
+         
+      }else
+      {
+          $this->setId(0);
+      }   
+  }
+    
+    public function obtener_tipo_punto() {
+        $this->obj_data_provider->conectar();
+        if($this->condicion==""){
+            $this->arreglo=$this->obj_data_provider->trae_datos(
+                    "T_TipoPuntoBCR", 
+                    "*",
+                    "");
+            $this->arreglo=$this->obj_data_provider->getArreglo();
+            $this->obj_data_provider->desconectar();
+            $this->resultado_operacion=true;
+        }
+        else{
+            $this->arreglo=$this->obj_data_provider->trae_datos(
+                    "T_TipoPuntoBCR", 
+                    "*",
+                    $this->condicion);
+            $this->arreglo=$this->obj_data_provider->getArreglo();
+            $this->obj_data_provider->desconectar();
+            $this->resultado_operacion=true;
+        }
+    }
+    
+    public function agregar_puntobcr_tp(){
+        $this->obj_data_provider->conectar();
+        $this->obj_data_provider->inserta_datos("T_TT_PuntoBCR", "ID_PuntoBCR, ID_Tipo_Punto", "'".$this->id2."','".$this->id."'");
+        $this->obj_data_provider->desconectar();
+    }
+    public function eliminar_relacion_puntobcr_tp(){
+        $this->obj_data_provider->conectar();
+        $this->obj_data_provider->eliminar_datos("T_TT_PuntoBCR", "ID_PuntoBCR='".$this->id2."' AND ID_Tipo_Punto='".$this->id."'");
+        $this->obj_data_provider->desconectar();
+    }
+    
+     public function agregar_nueva_tp(){
+        $this->obj_data_provider->conectar();
+        $this->obj_data_provider->inserta_datos("t_tipopuntobcr", "ID_Tipo_Punto, Tipo_Punto, Observaciones, Estado", "null,'".$this->tipo."','".$this->observaciones."','".$this->estado."'");
+        $this->obj_data_provider->desconectar();
+    }
+    
+    public function agregar_nueva_tp_para_prontuario(){
+        $this->obj_data_provider->conectar();
+        $this->obj_data_provider->inserta_datos_para_prontuario("t_tipopuntobcr", "ID_Tipo_Punto, Tipo_Punto, Observaciones, Estado", "null,'".$this->tipo."','".$this->observaciones."','".$this->estado."'");
+        $this->obj_data_provider->desconectar();
+    }
+    
+    //Este metodo realiza la modificación del estado del modulo, de activo a inactivo o viceversa en la bd
+    function edita_tp(){
+        $this->obj_data_provider->conectar();
+        //Llama al metodo para editar los datos correspondientes
+        $this->obj_data_provider->edita_datos("t_tipopuntobcr","Tipo_Punto='".$this->tipo."',Observaciones='".$this->observaciones."',Estado='".$this->estado."'",$this->condicion);
+        //Metodo de la clase data provider que desconecta la sesión con la base de datos
+        $this->obj_data_provider->desconectar();
+       
+    }
+    
+    //Este metodo realiza la modificación del estado del modulo, de activo a inactivo o viceversa en la bd
+    function edita_tp_para_prontuario(){
+        $this->obj_data_provider->conectar();
+        
+        //Llama al metodo para editar los datos correspondientes
+        $this->obj_data_provider->edita_datos_para_prontuario("t_tipopuntobcr","ID_Tipo_Punto='".$this->numero_tt."',Tipo='".$this->tipo."',Observaciones='".$this->observaciones."',Estado='".$this->estado."'",$this->condicion);
+        //Metodo de la clase data provider que desconecta la sesión con la base de datos
+        $this->obj_data_provider->desconectar();
+       
+    }
+
+     //Obtener el último id de evento para saber que se debe ingresar
+    function obtiene_id_ultima_tp_ingresada(){
+      //Establece la conexión con la bd
+      $this->obj_data_provider->conectar();
+      
+      $this->obj_data_provider->trae_datos("t_tipopuntobcr","max(ID_Tipo_Punto) ID_Tipo_Punto","");
+      
+      $this->arreglo2=$this->obj_data_provider->getArreglo();
+     
+      $this->obj_data_provider->desconectar();
+      
+      if (count($this->arreglo2)>0){
+          $this->setId_ultima_tp_ingresada($this->arreglo2[0]['ID_Tipo_Punto']);
+         
+      }else
+      {
+          $this->setId_ultima_tp_ingresada(0);
+      }   
+  }
+     //Este metodo realiza la modificación del estado del modulo, de activo a inactivo o viceversa en la bd
+    function edita_tp_de_personas(){
+        $this->obj_data_provider->conectar();
+        
+        //Llama al metodo para editar los datos correspondientes
+        $this->obj_data_provider->edita_datos("t_personal","ID_Tipo_Punto=".$this->id_ultima_tp_ingresada,$this->condicion);
+        //Metodo de la clase data provider que desconecta la sesión con la base de datos
+        $this->obj_data_provider->desconectar();
+       
+    }
+    
+     //Este metodo realiza la modificación del estado del modulo, de activo a inactivo o viceversa en la bd
+    function edita_tp_de_personas_para_prontuario(){
+        $this->obj_data_provider->conectar();
+        //Llama al metodo para editar los datos correspondientes
+        $this->obj_data_provider->edita_datos_para_prontuario("t_personal","ID_Tipo_Punto=".$this->id_ultima_tp_ingresada,$this->condicion);
+        //Metodo de la clase data provider que desconecta la sesión con la base de datos
+        $this->obj_data_provider->desconectar();
+       
+    }
+    
+      //Este metodo realiza la modificación del estado del modulo, de activo a inactivo o viceversa en la bd
+    function edita_tp_de_sitios_bcr(){
+        $this->obj_data_provider->conectar();
+        
+        //Llama al metodo para editar los datos correspondientes
+        $this->obj_data_provider->edita_datos("t_tt_puntobcr","ID_Tipo_Punto=".$this->id_ultima_tp_ingresada,$this->condicion);
+        //Metodo de la clase data provider que desconecta la sesión con la base de datos
+        $this->obj_data_provider->desconectar();
+       
+    }
+    
+      //Este metodo realiza la modificación del estado del modulo, de activo a inactivo o viceversa en la bd
+    function edita_tp_de_sitios_bcr_para_prontuario(){
+        $this->obj_data_provider->conectar();
+        
+        //Llama al metodo para editar los datos correspondientes
+        $this->obj_data_provider->edita_datos_para_prontuario("t_tt_puntobcr","ID_Tipo_Punto=".$this->id_ultima_tp_ingresada,$this->condicion);
+        //Metodo de la clase data provider que desconecta la sesión con la base de datos
+        $this->obj_data_provider->desconectar();
+       
+    }
+     function eliminar_tp_sobrantes_para_prontuario(){
+          
+        $this->obj_data_provider->conectar();
+        $this->obj_data_provider->eliminar_datos_para_prontuario("t_tipopuntobcr", $this->condicion);
+        $this->obj_data_provider->desconectar();
+  
+    }
+    function eliminar_tp_sobrantes(){
+          
+        $this->obj_data_provider->conectar();
+        $this->obj_data_provider->eliminar_datos("t_tipopuntobcr", $this->condicion);
+        $this->obj_data_provider->desconectar();
+  
+    }
+    function cambiar_estado_tp(){
+        
+        $this->obj_data_provider->conectar();
+        $this->obj_data_provider->edita_datos("t_tipopuntobcr","Estado='".$this->estado."'",$this->condicion);
+        $this->obj_data_provider->desconectar();
+    }
+ }?>
---
+++ vistas/plantillas/frm_tipo_telefono_catalogo.php
@@ -0,0 +1,122 @@
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Lista de Tipo Telefonos</title>
+        <?php require_once 'frm_librerias_head.html';?>
+        <link rel="stylesheet" href="vistas/css/ventanaoculta.css">
+ <script>
+            //Funcion para ocultar ventana de mantenimiento de proveedor
+            function ocultar_elemento(){
+                document.getElementById('ventana_oculta_1').style.display = "none";
+            }
+            //Valida informacion completa de formulario de proveedor
+            function check_empty() {
+                if (document.getElementById('nombre').value =="") {
+                    alert("Digita el nombre del Proveedor !");
+                } else {
+                    //alert("Form Submitted Successfully...");
+                    //Envia el formulario y lo oculta
+                    document.getElementById('ventana').submit();
+                    document.getElementById('ventana_oculta_1').style.display = "none";
+                }
+            }
+            //Funcion para agregar un nuevo tipo de telefono- formulario en blanco
+            function mostrar_agregar_tipo() {
+                document.getElementById('ID_Tipo_Telefono').value="0";
+                document.getElementById('nombre').value=null;
+                document.getElementById('observaciones').value=null;
+                document.getElementById('ventana_oculta_1').style.display = "block";
+            }
+            //Funcion para editar informacion de tipo telefono
+            function Editar_TT(id_tt,nomb,obser,esta){
+                document.getElementById('ID_Tipo_Telefono').value=id_tt;
+                document.getElementById('nombre').value=nomb;
+                document.getElementById('observaciones').value=obser;
+                $("#estado option[value="+esta+"]").attr("selected",true);
+                document.getElementById('ventana_oculta_1').style.display = "block";
+            };
+        </script>
+        
+         </head>
+         <body>
+   <?php require_once 'encabezado.php';?>
+        
+        <div class="container">
+        <h2>Listado General de Tipos Telefonos del BCR</h2>
+        <p>A continuación se detallan los diferentes Tipos de Telefonos que están registrados en el sistema:</p>            
+        <table id="tabla" class="display" cellspacing="0">
+          <thead>
+            <tr>
+              <th>ID_Telefono</th>
+              <th>Tipo de Telefono</th>
+              <th>Observaciones</th>
+              <th>Estado</th>
+              <th>Cambiar Estado</th>
+              <th>Mantenmiento</th>
+            </tr>
+          </thead>
+    <tbody>
+            <?php 
+            $tam=count($params);  
+            for ($i = 0; $i <$tam; $i++) {
+            ?>
+            <tr>
+                <td><?php echo $params[$i]['ID_Tipo_Telefono'];?></td>
+                <td><?php echo $params[$i]['Tipo_Telefono'];?></td>
+                <td><?php echo $params[$i]['Observaciones'];?></td>
+                
+                <?php if ($params[$i]['Estado']==1){?>  
+                    <td>Activo</td>
+                <?php }else {?>  
+                    <td>Inactivo</td>
+                <?php }?>
+           
+                  <td><a href="index.php?ctl=tipo_telefono_cambiar_estado&id=<?php echo $params[$i]['ID_Tipo_Telefono']?>&estado=<?php echo $params[$i]['Estado']?>">
+                    Activar/Desactivar</a></td>
+                
+             <td><a role="button" onclick="Editar_TT('<?php echo $params[$i]['ID_Tipo_Telefono'];?>','<?php echo $params [$i]['Tipo_Telefono'];?>',
+             '<?php echo $params [$i]['Observaciones'];?>',<?php echo $params [$i]['Estado'];?>)">
+                    
+                    Editar</a></td>
+                    
+            </tr>     
+            <?php } ?>
+            </tbody>
+        </table>
+        <a id="popup" onclick="mostrar_agregar_tipo()" class="btn btn-default" role="button">Agregar Nuevo Tipo</a>
+        </div>
+            <?php require 'vistas/plantillas/pie_de_pagina.php' ?>
+        
+       <!--agregar o editar-->
+        <div id="ventana_oculta_1"> 
+            <div id="popupventana">
+                <!--Formulario para proveedor de enlaces de telecomunicaciones-->
+                <form id="ventana" method="POST" name="form" action="index.php?ctl=tipo_telefono_guardar">
+                    <img id="close" src='vistas/Imagenes/cerrar.png' width="25" onclick ="ocultar_elemento()">
+                    <h2>Tipo Telefono</h2>
+                    <hr>
+                    
+                    <input hidden id="ID_Tipo_Telefono" name="ID_Tipo_Telefono" type="text">
+                    
+                    <label for="nombre">Tipo Telefono</label>
+                    <input class="form-control espacio-abajo" required id="nombre" name="nombre" placeholder="Tipo Telefono" type="text">
+                    
+                    <label for="observaciones">Observaciones</label>
+                    <input type="text" class="form-control espacio-abajo" id="observaciones" name="observaciones" placeholder="Observaciones del Tipo">
+                   
+                    <div class="form-group">
+                    <label for="sel1">Estado</label>
+                    <select class="form-control" id="estado" name="estado">
+                       
+                    <option value="1">Activo</option>
+                    <option value="0">Inactivo</option>
+                    
+                    </select>
+                    </div>
+                   <button><a href="javascript:%20check_empty()" id="submit">Guardar</a></button>
+                </form>
+            </div>
+        <!--Cierre agregar teléfono a Punto BCR-->
+        </div>
+    </body>
+</html>
